3274f48b0034aaa3cd547f91a3ea976385882492
Nabiullin Damir <nabiullindm@akbarsdigital.ru>
2023-06-14 15:04:33 +0300

    работа над ошибками

--- main.py
+++ main.py
@@ -1,77 +1,88 @@
-
-
 import math
 import random
 
+def test_greeting():
+    """
+    Напишите программу, которая выводит на экран приветствие.
+    """
+    name = "Анна"
+    age = 25
+    # TODO Сформируйте нужную строку
+    output = f"Привет, {name}! Тебе {age} лет."
+    # Проверяем результат
+    assert output == "Привет, Анна! Тебе 25 лет."
 
-def test_greetings():
-    # Напишите программу, которая выводит на экран приветствие.
-
-    name = 'Дамир'
-    age = '37'
-
-    text = f"Привет, меня зовут {name}. Мне {age} лет."
-    print(text)
-    assert text == "Привет, меня зовут Дамир. Мне 37 лет."
-
-test_greetings()
-
-def test_test_rectangle():
-    # Напишите программу, которая берет длину и ширину прямоугольника и считает его периметр и площадь.
-
-    length = 13
-    width = 15
-    perimetr = (length + width) * 2
-    area = length * width
-    text = f"периметр: {perimetr}, площадь: {width}"
-    print (text)
-    assert text == "периметр: 56, площадь: 15"
-
-test_test_rectangle()
 
 def test_rectangle():
-    #Напишите программу, которая берет радиус круга и выводит на экран его длину и площадь.
-    #Используйте константу PI
+    """
+    Напишите программу, которая берет длину и ширину прямоугольника
+    и считает его периметр и площадь.
+    """
+    a = 10
+    b = 20
 
-    radius = 21
+    # TODO сосчитайте периметр
+    perimeter = (a + b) * 2
+    assert perimeter == 60
 
-    area = math.pi * radius**2
-    perimetr = 2 * math.pi * radius
+    # TODO сосчитайте площадь
+    area = a * b
+    assert area == 200
 
-    text = f"Периметр: {perimetr}, площадь окружности: {area}"
-    assert text == "Периметр: 131.94689145077132, площадь окружности: 1385.4423602330987"
 
-    print(text)
+def test_circle():
+    """
+    Напишите программу, которая берет радиус круга и выводит на экран его длину и площадь.
+    Используйте константу PI
+    """
+    r = 23
+    # TODO сосчитайте площадь
+    area = math.pi * r ** 2
+    assert area == 1661.9025137490005
 
-test_rectangle()
+    # TODO сосчитайте длину окружности
+    length = 2 * math.pi * r
+    assert length == 144.51326206513048
+
 
 def test_random_list():
-    #Создайте список из 10 случайных чисел от 1 до 100 и отсортируйте его по возрастанию.
+    """
+    Создайте список из 10 случайных чисел от 1 до 100 и отсортируйте его по возрастанию.
+    """
 
-    random_list = sorted(random.sample(range(1, 100), 10))
+    # TODO создайте список
+    l = []
+    l = sorted(random.sample(range(1, 100), 10))
+    assert len(l) == 10
+    assert l[0] < l[-1]
 
-    print(random_list)
-
-    assert len(random_list) == 10
-    assert random_list[0] < random_list[-1]
-
-test_random_list()
 
 def test_unique_elements():
-#Удалите из списка все повторяющиеся элементы
+    """
+    Удалите из списка все повторяющиеся элементы
+    """
+    l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
+    # TODO удалите повторяющиеся элементы
 
-    spisok = [42, 3, 4, 4, 4, 2, 55, 44, 23, 12]
-    spisok2 = list(set(spisok))
-    print(spisok2)
-    assert spisok2 == [2, 3, 4, 42, 44, 12, 23, 55]
-test_unique_elements()
+    list = list(set(l))
+
+    assert isinstance(l, list)
+    assert len(l) == 10
+    assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 
 def test_dicts():
-#Создайте словарь из двух списков. Используйте первый список как ключи, а второй - как значения.
-#Выведите на экран все значения словаря. Подсказка: используй встроенную функцию zip.
-    sp1 = [3, 4, 5, 6]
-    sp2 = ['a', 's', 'd', 'b']
-    sp12 = list(zip(sp1, sp2))
-    print(sp12)
-    assert sp12 ==[(3, 'a'), (4, 's'), (5, 'd'), (6, 'b')]
-test_dicts()
+    """
+    Создайте словарь из двух списков.
+    Используйте первый список как ключи, а второй - как значения.
+    Выведите на экран все значения словаря.
+    Подсказка: используй встроенную функцию zip.
+    """
+    first = ["a", "b", "c", "d", "e"]
+    second = [1, 2, 3, 4, 5]
+    # TODO создайте словарь
+    d = {}
+    d = list(zip(first, second))
+
+    assert isinstance(d, dict)
+    assert len(d) == 5
