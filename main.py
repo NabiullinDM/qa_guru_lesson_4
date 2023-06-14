53e13014063e17b12f394461a85dc4c9db073899
Nabiullin Damir <nabiullindm@akbarsdigital.ru>
2023-06-14 09:02:12 +0300

    первое задание

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+import math
+import random
+
+
+def test_greetings():
+    # Напишите программу, которая выводит на экран приветствие.
+
+    name = 'Дамир'
+    age = '37'
+
+    text = "Привет, меня зовут {name}. Мне {age} лет."
+    print(text)
